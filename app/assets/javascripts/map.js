59c27fa01f195246dfe3e5e05b5e4f979aab117b
Will Potter <willstonepotter@gmail.com>
2014-06-15 19:10:31 -0400

    adding stuff to gitignore

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -3,6 +3,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require underscore
+//= require bootstrap
 //= require moment
 //= require turbolinks
 //= require leaflet
@@ -10,44 +11,74 @@
 
 $(document).ready(function() {
   var stations = {};
+  var colors = ['rgb(255,247,251)','rgb(236,231,242)','rgb(208,209,230)','rgb(166,189,219)','rgb(116,169,207)','rgb(54,144,192)','rgb(5,112,176)','rgb(4,90,141)','rgb(2,56,88)'];
+  $('.btn').button();
+  $(".btn").click(function() {
+    var action = $(this).children("input").attr("id");
+    if(action == "open_bikes") {
+      setColor(stations, "bikes");
+    } else if(action == "open_docks") {
+      setColor(stations, "docks");
+    }
+  });
+
 
   function calculateColor(val) {
     val = parseInt(val, 10);
-    if (val == 0) {
-      return "rgba(255, 81, 81, 1)";
-    } else if (val < 3) {
-      return "rgba(255, 81, 81, 0.7)";
-    } else if (val < 6) {
-      return "rgba(255, 238, 81, 0.7)";
-    } else if (val < 9) {
-      return "rgba(209, 255, 81, 0.7)";
-    } else {
-      return "rgba(100, 255, 81, 0.7)";
+    if (val >= 8) {
+      val = 7;
     }
+    return colors[val];
   }
 
-  map = L.map('map').setView([42.349624, -71.083603], 13);
+  function generateColorLegend(colors) {
+    _.each(colors, function(c, i) {
+      $("#legend table").append("<tr><td class='text'>" + i + "</td><td><span class='sample' style='background:" + c + "'></span></td></tr>");
+    });
+  }
+
+  function setColor(stations, type) {
+    _.each(stations, function(s) {
+      var value;
+      if (type == "bikes") {
+        value = s.bikes;
+      } else {
+        value = s.docks;
+      }
+      s.marker.setStyle({
+        fillColor: calculateColor(value)
+      });
+    });
+  }
+
+  generateColorLegend(colors);
+  map = L.map('map').setView([42.349624, -71.083603], 13).on("zoomend", function(e) {
+    var zoom = this.getZoom();
+    var markerSize = 5 + (zoom - 12) * 2;
+    _.each(stations, function(s) {
+      s.marker.setRadius(markerSize);
+    });
+  });
   L.tileLayer('http://{s}.tiles.mapbox.com/v3/willpots.ih11j74m/{z}/{x}/{y}.png', {
     attribution: 'Map by <a href="http://twitter.com/willpots">@willpots</a>. Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     minZoom: 12,
     maxBounds: L.latLngBounds(L.latLng(42.257841, -71.268311), L.latLng(42.447465, -70.923615))
   }).addTo(map);
-
   $.getJSON("/data/capacity.json", {}, function(result) {
 
     // Check if station exists in our global station object
     var lastUpdate = new Date(parseInt(result.stations.lastUpdate, 10));
-    console.log(result.stations);
     $(".lastUpdate").html("Last Updated " + moment(lastUpdate).fromNow());
     _.each(result.stations.station, function(c) {
       if (!(c.id in stations)) {
         stations[c.id] = {
           marker: L.circleMarker([parseFloat(c.lat, 10), parseFloat(c.long, 10)], {
-            stroke: false,
-            opacity: 0,
-            fillColor: calculateColor(c.nbBikes),
-            fillOpacity: 1
+            stroke: true,
+            color: "#333",
+            opacity: 0.8,
+            fillOpacity: 1,
+            radius: 8
           }).bindPopup(c.name).on('mouseover', function(e) {
             //open popup;
             this.openPopup();
@@ -56,6 +87,7 @@ $(document).ready(function() {
           bikes: parseInt(c.nbBikes, 10),
           docks: parseInt(c.nbEmptyDocks, 10)
         };
+        setColor(stations, "bikes");
       } else {
 
       }
